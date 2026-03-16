9161d5d2c95b735443e6f3b7ecc420d9da295801
V1ane24 <vane.cartagena24@gmail.com>
2026-03-16 11:31:56 -0600

    Se agreagron las clases de usuarioy rol de la capa EN

---
+++ SysSeguridad2G05.EN/Rol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SysSeguridad2G05.EN
+{
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre de rol es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        public List<Usuario> Usuarios { get; set; } = null!;
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+}
---
+++ SysSeguridad2G05.EN/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SysSeguridad2G05.EN
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name ="Rol")]
+        public int IdRol { get; set; }
+
+        [Required(ErrorMessage ="El nombre de usuario es obligatorio")]
+        [StringLength(40, ErrorMessage ="Maximo 40 caracteres")]
+        [Display(Name ="Nombre Usuario")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido de usuario es obligatorio")]
+        [StringLength(40, ErrorMessage = "Maximo 40 caracteres")]
+        [Display(Name = "Apellido Usuario")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El Login de usuario es obligatorio")]
+        [StringLength(200, ErrorMessage = "Maximo 200 caracteres")]
+        [Display(Name = "Login Usuario")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Password de usuario es obligatorio")]
+        [StringLength(40, ErrorMessage = "Maximo 40 caracteres")]
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Estado es obligatorio")]
+        public byte Status { get; set; }
+
+        [Display(Name ="Fecha Registro")]
+        public DateTime FechaRegistro { get; set; }
+        public Rol Rol { get; set; }
+        public int Top_Aux { get; set; }
+        public string ConfirmPassword_aux { get; set; }
+    }
+}
